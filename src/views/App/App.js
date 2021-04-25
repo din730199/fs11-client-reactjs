a4c313543938b2c0a81a5f10ca3deef7267cc63f
nhan <dinhnhan3007@gmail.com>
2021-04-25 12:30:43 +0700

    dsa

--- src/views/App/App.js
+++ src/views/App/App.js
@@ -46,6 +46,13 @@ function App() {
 
               {/* <!-- End Page Content --> */}
             </div>
+            <footer class="sticky-footer bg-white">
+              <div class="container my-auto">
+                <div class="copyright text-center my-auto">
+                  <span>Copyright &copy; Fs11-client</span>
+                </div>
+              </div>
+            </footer>
           </div>
           {/* End Content Wrapper */}
         </Router>
@@ -83,8 +90,16 @@ function App() {
 
             {/* <!-- End Page Content --> */}
           </div>
+          <footer class="sticky-footer bg-white">
+            <div class="container my-auto">
+              <div class="copyright text-center my-auto">
+                <span>Copyright &copy; Fs11-client</span>
+              </div>
+            </div>
+          </footer>
         </div>
         <LogoutModal />
+
         {/* End Content Wrapper */}
       </Router>
     </div>
